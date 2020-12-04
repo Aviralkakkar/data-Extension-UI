278bd318e9ca95c67a8b4ba41bdc16e10f107b98
Aviralkakkar <aviralkakkar3@gmail.com>
2020-12-04 14:25:51 +0530

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-
 const express = require("express");
 const app = express();
-var http = require('http');
-var fs = require('fs');
-
 let port = process.env.PORT || 3000;
 
-fs.readFile('./public/index.html', function (err, html) {
+const importData = require("./public/index.html");
 
-  if (err) throw err;    
-
-  http.createServer(function(request, response) {  
-      response.writeHeader(200, {"Content-Type": "text/html"});  
-      response.write(html);  
-      response.end();  
-  }).listen(port);
+app.get("/", (req,res) => {
+    res.send("importData");
 });
 
-//app.get("/", (req,res) => {
- //   res.send("Hello World");
-//});
-
-//app.listen(port, () => {
-//    console.log('Example app is listening on port http://localhost:${port}');
-//});
+app.listen(port, () => {
+    console.log('Example app is listening on port http://localhost:${port}');
+});
