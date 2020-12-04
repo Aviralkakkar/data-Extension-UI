c2bc1e9360f15c540eb981fc1497ef070128f82d
Aviralkakkar <aviralkakkar3@gmail.com>
2020-12-04 12:48:47 +0530

    first commit

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
-'use strict';
-// Module Dependencies
-// -------------------
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var errorhandler = require('errorhandler');
-var http        = require('http');
-var path        = require('path');
-var request     = require('request');
-//var routes      = require('./routes');
-//var activity    = require('./routes/activity');
 
-var app = express();
+const express = require("express");
+const app = express();
+var http = require('http');
+var fs = require('fs');
 
-// Configure Express
-app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json({type: 'application/json'})); 
-//app.use(bodyParser.urlencoded({ extended: true }));
+let port = process.env.PORT || 3000;
 
-//app.use(express.methodOverride());
-//app.use(express.favicon());
+fs.readFile('./public/index.html', function (err, html) {
 
-app.use(express.static(path.join(__dirname, 'public')));
+  if (err) throw err;    
 
-// Express in Development Mode
-if ('development' == app.get('env')) {
-  app.use(errorhandler());
-}
-
-// HubExchange Routes
-app.get('/', routes.index );
-app.post('/login', routes.login );
-app.post('/logout', routes.logout );
-
-// Custom Hello World Activity Routes
-//app.post('/journeybuilder/save/', activity.save );
-//app.post('/journeybuilder/validate/', activity.validate );
-//app.post('/journeybuilder/publish/', activity.publish );
-//app.post('/journeybuilder/execute/', activity.execute );
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  http.createServer(function(request, response) {  
+      response.writeHeader(200, {"Content-Type": "text/html"});  
+      response.write(html);  
+      response.end();  
+  }).listen(port);
 });
+
+//app.get("/", (req,res) => {
+ //   res.send("Hello World");
+//});
+
+//app.listen(port, () => {
+//    console.log('Example app is listening on port http://localhost:${port}');
+//});
