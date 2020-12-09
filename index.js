f472c80272bbe41df2eefbd9f9fdba2a479cae4e
Aviralkakkar <aviralkakkar3@gmail.com>
2020-12-09 18:33:49 +0530

    activity update

--- index.js
+++ index.js
@@ -44,6 +44,33 @@ request.post({
        console.log("Access"+body.access_token);
        console.log("response" + response);
 
+       var data='<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Actions:mustUnderstand="1">Retrieve</a:Action><a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID><a:ReplyTo>       <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>      <fueloauth xmlns="http://exacttarget.com">'+body.access_token+'</fueloauth></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">     <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI"> <RetrieveRequest><ObjectType>DataExtension</ObjectType> <Properties>CustomerKey</Properties><Properties>Name</Properties><Properties>DataExtension.ObjectID</Properties> <Properties>IsSendable</Properties>  <Properties>CategoryID</Properties>  	<Filter xsi:type="SimpleFilterPart"> <Property>CategoryID</Property><SimpleOperator>equals</SimpleOperator><Value>29130</Value></Filter>    </RetrieveRequest> </RetrieveRequestMsg> </s:Body></s:Envelope>';
+				data=JSON.stringify(data);
+				request.post({
+				headers: {
+					'content-type': 'text/xml',
+					'Authorization':'Bearer '+body.access_token,
+				},
+				url: 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx',
+				body:data,
+				json:true
+				},function(error2,response2,body2){
+					
+					console.log('\nSOAP Body'+(body2));
+					console.log('\nSOAP Error'+(error2));
+					console.log('\nSOAP Response'+(response2));
+					console.log('\nSOAP Body'+JSON.stringify(body2));
+					console.log('\nSOAP Error'+JSON.stringify(error2));
+					console.log('\nSOAP Response'+JSON.stringify(response2));
+					
+				})
+			
+				const ind2 = path.join(__dirname, 'public', 'SFMC-DE.html');
+				res.sendFile(ind2);
+				console.log("Access" + body.access_token);
+				console.log("response" + response);
+				console.log(clientSec);
+				//  res.send(clientSec);   
       
 
     //   if(accTok == null && accTok == "undefined" )
@@ -104,18 +131,13 @@ request.post({
                     */
         //        }) 
                   
-        app.post('/PostData', (req, res) => {
-          
-          console.log("clientSecret : "+req.body.clientSecret);
-          console.log("clientId : "+req.body.clientSecret);
-          console.log("authUrl : "+req.body.authenticationBaseURI);
-        })
 
       });
   //    app.get('/', routes.activity );
   //    app.post('/login', routes.login );
   // console.log("yeh account token hai : " + accTok);
   // res.send(accTok);   
+ //   res.redirect('/');
 
 })
 
